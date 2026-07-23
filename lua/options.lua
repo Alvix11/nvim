b819e3fbf833e274c89d94491900168ed91ba693
Alvix11 <alvix.wick@gmail.com>
2026-07-23 16:17:01 -0400

    feat: enable treesitter code folding

--- lua/options.lua
+++ lua/options.lua
@@ -6,3 +6,10 @@ local o = vim.o
 o.relativenumber = true
 o.cursorline = true
 o.cursorlineopt = "both"
+
+o.foldmethod = "expr"
+o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+o.foldenable = true
+o.foldlevel = 99
+o.foldlevelstart = 99
+o.foldcolumn = "1"
