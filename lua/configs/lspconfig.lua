7b48d869db3052f2fe4b8f8cd3d4bb75a21b8b7a
Alvix11 <alvix.wick@gmail.com>
2026-07-24 00:29:13 -0400

    feat: add config odoo

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,5 +1,25 @@
 require("nvchad.configs.lspconfig").defaults()
 
+vim.lsp.config("odoo_ls", {
+  cmd = {
+    vim.fn.expand("$HOME/.local/share/nvim/odoo/odoo_ls_server"),
+  },
+  filetypes = { "python", "xml" },
+  workspace_folders = {
+    {
+      uri = vim.uri_from_fname(vim.fn.getcwd()),
+      name = "main_folder",
+    },
+  },
+  settings = {
+    Odoo = {
+      selectedProfile = "main",
+    },
+  },
+})
+
+vim.lsp.enable({ "odoo_ls" })
+
 local servers = { "html", "cssls", "rust_analyzer", "basedpyright", "ruff", "ts_ls", "lemminx", "lua_ls" }
 vim.lsp.enable(servers)
 
