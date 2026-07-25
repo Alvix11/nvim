3e06fe7598413680bf04a65480b1b4bc499ac0cc
Alvix11 <alvix.wick@gmail.com>
2026-07-25 00:03:51 -0400

    chore: delete comments AI

--- lua/plugins/mini-icons.lua
+++ lua/plugins/mini-icons.lua
@@ -1,15 +1,12 @@
 return {
   "echasnovski/mini.icons",
-  lazy = false, -- Cargar al arrancar para interceptar las peticiones de íconos
+  lazy = false,
   opts = {
     style = "glyph",
   },
   config = function(_, opts)
     local mini_icons = require "mini.icons"
     mini_icons.setup(opts)
-
-    -- Engaña a Neovim para que cualquier plugin que pida `nvim-web-devicons`
-    -- reciba en su lugar los íconos de mini.icons:
     mini_icons.mock_nvim_web_devicons()
   end,
 }
